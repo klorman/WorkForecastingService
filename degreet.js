ff39943d770f77488ee01f89a600d1b89d995e0b
MinimusBlog <126412617+MinimusBlog@users.noreply.github.com>
2023-05-21 17:36:39 +0300

    Add files via upload

---
+++ degreet.js
@@ -0,0 +1,11 @@
+const hours = new Date().getHours()
+let message
+
+if (hours <12)
+    message = 'Доброе утро!'
+    else message = 'Добрый день!'
+    if (hours >16) message = 'Добрый вечер!'
+
+    messageSpan.innerText = message
+    
+AOS.init();
